863dbc64e7d1fb11b9f31193695eecda605b8cab
LynnykArtem <97697664+LynnykArtem@users.noreply.github.com>
2022-01-18 22:03:09 +0200

    Add files via upload

---
+++ Калькулятор2.py
@@ -0,0 +1,24 @@
+
+input_value1=int(input("Введите первое число:"))
+input_value2=int(input("Введите второе число:"))
+operator=input("Что делаем?(+,-,*,/):")
+if operator == "+":
+    result = input_value1 + input_value2
+    print("Результат:" + str(result))
+elif operator=="-":
+    result=input_value1-input_value2
+    print("Результат:" + str(result))
+elif operator=="*":
+    result=input_value1*input_value2
+    print("Результат:" + str(result))
+elif operator=="/":
+    if input_value2 == 0:
+        print("Деление на ноль!!!")
+    else:
+        result=input_value1/input_value2
+    print("Результат:" + str(result))
+else:
+    print("Выбрана неверная операция!")
+
+
+
